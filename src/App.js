356745363412e6e8eada763d4bd70ffefa917c8f
mTarik12 <tarikmayula@gmail.com>
2021-01-14 17:49:28 +0200

    Directory component, ti fix node-sass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import './App.css';
-import HomePage from './homepage.component';
+import React from "react";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
-  return <HomePage/>;
+  return <HomePage />;
 }
 
 export default App;
