bc6555fa3862f6e7a9f0ef17a8e49938b14910a9
mTarik12 <72448312+mTarik12@users.noreply.github.com>
2021-02-13 22:16:42 +0200

    added photos for shop and collections

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom'
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from './pages/shop/shop.component';
 
 const HatPage = () => (
   <div>
@@ -18,7 +19,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop/hats' component={HatPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
