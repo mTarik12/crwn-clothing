5761cd28aa53e4599aa47a88454c3a09152958fc
mTarik12 <72448312+mTarik12@users.noreply.github.com>
2021-02-22 22:32:02 +0200

    sign in button component + Form input component

--- src/App.js
+++ src/App.js
@@ -5,15 +5,8 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component.jsx';
 import Header from './components/header/header.component';
-
-const HatPage = () => (
-  <div>
-    <h1>
-      Hats Page
-    </h1>
-  </div>
-);
 
 function App() {
   return (
@@ -22,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
